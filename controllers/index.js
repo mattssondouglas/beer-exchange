2387a6725b1bd41636c8b6c25cdee2b069be3a17
kazomikun <106589367+kazomikun@users.noreply.github.com>
2022-07-25 06:19:07 +0100

    Create index.js

---
+++ controllers/index.js
@@ -0,0 +1,20 @@
+// Import Packages
+const express = require('express')
+const router = express.Router()
+
+// Create POST controller
+
+// Create GET controller
+router.get('/', async (req, res) => {
+  res.redirect('/orders')
+})
+
+// Create PATCH controller
+
+// Create DELETE controller
+
+// Views
+// Create here a controller that accepts GET requests and renders the "search" page
+
+// Export module
+module.exports = router
