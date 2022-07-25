c8d18e19f291e5353d1cae60bb93b30940b902d8
Douglas <61967200+mattssondouglas@users.noreply.github.com>
2022-07-25 15:16:36 +0700

    add beers model

---
+++ models/beers.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+module.exports = mongoose.model('beers', {
+  name: {
+    type: String,
+    required: true
+  },
+
+  startingPrice: {
+    type: Number,
+    required: true
+  },
+
+  minimumPrice: {
+    type: Number,
+    required: true
+  },
+
+  photo: String
+})
