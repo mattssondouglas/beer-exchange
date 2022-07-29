ae06b25c2248240a6843fbdb40e9ea34361dc3ce
kazomikun <106589367+kazomikun@users.noreply.github.com>
2022-07-29 04:15:27 +0100

    Update orders.js

--- controllers/orders.js
+++ controllers/orders.js
@@ -7,16 +7,6 @@ const Beers = require('../models/beers')
 
 // Create POST controller
 router.post('/', async (req, res) => {
-  // no authentication planned at present
-  // if (req.isAuthenticated()) {
-  // } else {
-  //   res.redirect('/auth/login')
-  // }
-
-  // let check = await Orders.findOne({
-  //   beer: '62df7bfaf9179b0bb64d569b'
-  // })
-
   let beer = await Beers.findOne({
     _id: req.body.id
   })
@@ -37,31 +27,11 @@ router.post('/', async (req, res) => {
     }
   )
 
-  // let history = await History.create({
-  //   beers: [
-  //     {
-  //       beer: order.beer,
-  //       currentPrice: order.price,
-  //       lowestPrice: order.price,
-  //       highestPrice: order.price
-  //     }
-  //   ]
-  // })
-
-  // let history = await History.findOneAndUpdate({
-  // 	beers.beer = order.beer
-  // }, {
-  // 	beers.currentPrice: 50
-  // })
-
   res.redirect('/')
 })
 
 // Create GET controller
 router.get('/', async (req, res) => {
-  // let test = { beer: '62de495d2d09db30945b410a', price: 35 }
-  //
-  // let order = await Orders.create(test)
   //
   res.render('orders')
 })
