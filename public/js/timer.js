916c5d4e45fe269ad0a96c48dfdecda83f8bb00e
Douglas <61967200+mattssondouglas@users.noreply.github.com>
2022-07-29 14:09:28 +0700

    add timer method

--- public/js/timer.js
+++ public/js/timer.js
@@ -1,6 +1,22 @@
-setInterval(myTimer, 1000)
-function myTimer() {}
-// For Emergency, stop timer after 20 seconds
-setTimeout(() => {
-  clearInterval(myTimer)
-}, 20 * 1000)
+// setInterval(myTimer, 1000)
+// function myTimer() {}
+// // For Emergency, stop timer after 20 seconds
+// setTimeout(() => {
+//   clearInterval(myTimer)
+// }, 20 * 1000)
+
+const updateBeers = async () => {
+  console.log('hi')
+}
+
+updateBeers()
+
+let decrease = setInterval(() => {
+  updateBeers()
+}, 1 * 1000)
+
+// Stop after 60 seconds
+
+setInterval(() => {
+  clearInterval(decrease)
+}, 5 * 1000)
