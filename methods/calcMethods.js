9c6adade7b6445c76b7c5ce7cfcc3946b08c2f11
kazomikun <106589367+kazomikun@users.noreply.github.com>
2022-07-29 04:16:20 +0100

    Create calcMethods.js

---
+++ methods/calcMethods.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+
+const priceDrop = beer => {
+  beer.currentPrice = (beer.currentPrice * 0.98).toFixed(2)
+  return beer.currentPrice
+}
+
+const marketCrash = beer => {
+  if (beer.currentPrice >= beer.startingPrice) {
+    beer.currentPrice = (beer.currentPrice * 0.5).toFixed(2)
+  } else if (beer.currentPrice < beer.startingPrice) {
+    beer.currentPrice = (beer.minimumPrice * 1.2).toFixed(2)
+  }
+  return beer.currentPrice
+}
+
+// Export module
+module.exports = { marketCrash, priceDrop }
