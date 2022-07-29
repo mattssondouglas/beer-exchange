e6170846eca756b9a1a43353f71950bb28279fda
kazomikun <106589367+kazomikun@users.noreply.github.com>
2022-07-29 10:56:57 +0100

    worked on timer function to fetch data
    still needs to update HTML at intervals

--- public/js/timer.js
+++ public/js/timer.js
@@ -1,6 +1,44 @@
-setInterval(myTimer, 1000)
-function myTimer() {}
-// For Emergency, stop timer after 20 seconds
-setTimeout(() => {
-  clearInterval(myTimer)
-}, 20 * 1000)
+// import { Beers } from '../models/beers'
+// console.log('start')
+document.querySelectorAll('.price').innerHTML = 'Hi'
+
+const decreasePrices = () => {
+  // console.log('decrease prices')
+}
+
+const updateBeers = async () => {
+  // decreasePrices()
+  // console.log('updateBeers')
+  // console.log('hi')
+
+  fetch('/beers/decrease', {
+    method: 'PATCH'
+  })
+    .then(response => response.json())
+    .then(beers => {
+      console.log('beers', beers)
+      beers.forEach(beer => {
+        console.log('beer', beer)
+        // update beers price in the UI
+        document.querySelectorAll('.price').innerHTML =
+          '<span>${beer.price}</span>'
+      })
+    })
+    .catch(err => {
+      console.log('err', err)
+    })
+
+  // console.log(beers)
+}
+
+updateBeers()
+
+// let decrease = setInterval(() => {
+//   updateBeers()
+// }, 1 * 1000)
+
+// Stop after 60 seconds
+
+// setInterval(() => {
+//   clearInterval(decrease)
+// }, 5 * 1000)
