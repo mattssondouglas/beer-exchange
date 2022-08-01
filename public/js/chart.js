cb418c258b7e1e02946f535e8f27c4d2fbad442c
Douglas <61967200+mattssondouglas@users.noreply.github.com>
2022-08-01 17:42:15 +0700

    add second dummy dataset, modify names and values, add comments

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,13 +1,19 @@
-const labels = ['January', 'February', 'March', 'April', 'May', 'June']
+const labels = ['Hour 1', 'Hour 2', 'Hour 3', 'Hour 4', 'Hour 5', 'Hour 6']
 
 const data = {
   labels: labels,
   datasets: [
     {
-      label: 'My First dataset',
+      label: 'Singha',
       backgroundColor: 'rgb(255, 99, 132)',
       borderColor: 'rgb(255, 99, 132)',
       data: [0, 10, 5, 2, 20, 30, 45]
+    },
+    {
+      label: 'Leo',
+      backgroundColor: 'rgb(0, 99, 132)',
+      borderColor: 'rgb(0, 99, 132)',
+      data: [10, 1, 15, 12, 2, 13, 40]
     }
   ]
 }
@@ -21,3 +27,4 @@ const config = {
 const myChart = new Chart(document.getElementById('myChart'), config)
 
 // make route (which one?) res.json instead of rendering some page
+// for each beer, create a dataset. in each dataset, get datapoints from history.currentPrice (use a loop)
