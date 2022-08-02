174e8c248466cc2c5ce315376fa230c9dbc38dc9
Douglas <61967200+mattssondouglas@users.noreply.github.com>
2022-08-02 16:02:20 +0700

    restructure chart.js to update dynamically

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,40 +1,94 @@
-// Fetch
-const getChartData = async () => {
+const createChart = data => {
+  const config = {
+    type: 'line',
+    data: data,
+    options: {}
+  }
+
+  const myChart = new Chart(document.getElementById('myChart'), config)
+
+  // 4. use data to create config
+  // {
+  // 	data...
+  // }
+  // 5. code that creates chart
+  // document.querySelector('#chartId')
+}
+
+const getDataFromAPI = async () => {
+  // 1. get data from API
+  // let rawData = await fetch('/yourURL')
   const url = '/chartdata'
   const response = await fetch(url)
-  const datapoints = await response.json()
-  console.log(datapoints)
-  return datapoints
+  const rawData = await response.json()
+  // 2. transform the data from controller into chart.js format
+  // data = rawData.map(e => {
+  // etc.
+  // })
+
+  // const labels = ['Hour 1', 'Hour 2', 'Hour 3', 'Hour 4', 'Hour 5', 'Hour 6']
+  const labels = rawData.map(history => history.timestamp)
+
+  let beers = rawData.map(history => history.beers.map(beer => beer))
+
+  let beerNames = rawData.map(history => history.beers.map(beer => beer.name))
+
+  let beerPrices = rawData.map(history =>
+    history.beers.map(beer => beer.currentPrice)
+  )
+
+  console.log(
+    rawData.map(history =>
+      history.beers.map(beer => {
+        return {
+          label: beerNames,
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: beerPrices
+        }
+      })
+    )
+  )
+
+  const data = {
+    labels: labels,
+    datasets: rawData.map(history =>
+      history.beers.map(beer => {
+        return {
+          label: beerNames,
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: beerPrices
+        }
+      })
+    )
+
+    // [
+    //   {
+    //     label: beerNames[0],
+    //     backgroundColor: 'rgb(255, 99, 132)',
+    //     borderColor: 'rgb(255, 99, 132)',
+    //     data: [
+    //       beer.currentPrice[0],
+    //       beer.currentPrice[1],
+    //       beer.currentPrice[2],
+    //       2,
+    //       20,
+    //       30,
+    //       45
+    //     ]
+    //   },
+    //   {
+    //     label: 'Leo',
+    //     backgroundColor: 'rgb(0, 99, 132)',
+    //     borderColor: 'rgb(0, 99, 132)',
+    //     data: [10, 1, 15, 12, 2, 13, 40]
+    //   }
+    // ]
+  }
+
+  // 3. pass the data to the chart
+  createChart(data)
 }
-getChartData()
 
-const labels = ['Hour 1', 'Hour 2', 'Hour 3', 'Hour 4', 'Hour 5', 'Hour 6']
-
-const data = {
-  labels: labels,
-  datasets: [
-    {
-      label: 'Singha',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
-      data: [0, 10, 5, 2, 20, 30, 45]
-    },
-    {
-      label: 'Leo',
-      backgroundColor: 'rgb(0, 99, 132)',
-      borderColor: 'rgb(0, 99, 132)',
-      data: [10, 1, 15, 12, 2, 13, 40]
-    }
-  ]
-}
-
-const config = {
-  type: 'line',
-  data: data,
-  options: {}
-}
-
-const myChart = new Chart(document.getElementById('myChart'), config)
-
-// make route (which one?) res.json instead of rendering some page
-// for each beer, create a dataset. in each dataset, get datapoints from history.currentPrice (use a loop)
+getDataFromAPI()
